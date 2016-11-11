f4dc6820305bb8cff86af86c3541d36c770b6bbb
Jamis Charles <jacharles@paypal.com>
2016-11-10 22:47:47 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,26 @@
+
+module.exports = {
+
+    plugins: ["this"], //eslint-plugin-this
+
+	rules: {
+        "this/no-this": "error",
+		"no-console": "off",
+		"indent": [
+			"error",
+			2
+		],
+		"linebreak-style": [
+			"error",
+			"unix"
+		],
+		"quotes": [
+			"error",
+			"single"
+		],
+		"semi": [
+			"error",
+			"always"
+		]
+    }
+};
